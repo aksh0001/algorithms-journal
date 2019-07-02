1859117a7a4b26d89c1b10819835c6d75f2d166a
Akshay Kumar <aksh0001@student.monash.edu>
2019-07-03 03:05:50 +1000

    Added jump game 3 (DP solution)

---
+++ questions/linkedlists/Linked List Cycle.py
@@ -0,0 +1,16 @@
+"""
+Module for detecting cycles in linked lists
+@author a.k
+"""
+from data_structures.LinkedList import *
+
+
+def has_cycle(head: ListNode) -> bool:
+    """
+    True if a cycle is present in the linked list
+    :param head: head of the linked list
+    :return: true if cycle present; false, otherwise.
+    :Time:
+    :Space:
+    """
+    
