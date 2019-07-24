26cbe009041bdf9453e570179288a23e046d06e3
Akshay Kumar <aksh0001@student.monash.edu>
2019-07-24 21:12:50 +0530

    Added get() methods and contains() for binary tree implementation

--- data_structures/trees/BinaryTree.py
+++ data_structures/trees/BinaryTree.py
@@ -45,7 +45,7 @@ class BinaryTree:
             if removed.left is None:
                 removed.left = node
                 return
-            else:  # If a left child exists, enqueue it
+            else:  # If a left child exists, enqueue it to process it
                 bfs_queue.put(removed.left)
 
             # Similarly for the right child
@@ -55,21 +55,72 @@ class BinaryTree:
             else:
                 bfs_queue.put(removed.right)
 
-    def get(self, key: TreeNode.key) -> TreeNode:
-        return self._get(self.root, key)
+    def get(self, key) -> TreeNode:
+        ret_val = self._get(self.root, key)
+        if ret_val is None:
+            raise LookupError("Error! Key doesn't exist!")
+        else:
+            return ret_val
 
-    def _get(self, root: TreeNode, key: TreeNode.key) -> TreeNode:
+    def _get(self, root: TreeNode, key: TreeNode) -> TreeNode:
+        """
+        Helper get method that pre-order traverses tree to get the node associated with key
+        :param root: root of tree
+        :param key: key to look for
+        :return: None if not found, the node, otherwise
+        :Time: O(N)
+        :Space: O(N)
+        """
+        # Always do the edge-case check, which could raise an error, FIRST!!
+        if root is None:  # BC2 - not found
+            return None
+        # Pre-order traverse: examine root first, then recur left, then recur right
         if root.key == key:  # BC1 - found
             return root
-        if root is None:  # BC2 - not found
-            raise LookupError("Error! Key doesn't exist!")
+
+        result_left_subtree = self._get(root.left, key)
+        result_right_subtree = self._get(root.right, key)
+        if result_left_subtree is not None:
+            return result_left_subtree
+        elif result_right_subtree is not None:
+            return result_right_subtree
+        else:
+            return None
+
+    def contains(self, key) -> bool:
+        """
+        Returns whether the tree contains a given key, this time using level-order traversal (standard BFS)
+        :param key: key to be checked
+        :return: whether the tree contains the key
+        """
+        # BFS (level-order traversal) algorithm
+        bfs_queue = Queue()
+        bfs_queue.put(self.root)  # Enqueue source node and mark as visited
+
+        while not bfs_queue.empty():
+            removed = bfs_queue.get()  # Dequeue from front
+            if removed.key == key:  # If the removed node matches, return
+                return True
+            # Add the removed vertex's adjacent unmarked nodes into the queue and mark them
+            # But as in the standard BFS, we only add the adjacent vertices that exist (i.e. not None)
+            # Since in the adjacency list rep', all vertices adjacent to a vertex in the list, exist by definition
+
+            if removed.left is not None:
+                bfs_queue.put(removed.left)  # mark visited
+            if removed.right is not None:
+                bfs_queue.put(removed.right)  # mark visited
+        return False
 
 
 if __name__ == "__main__":
     test = BinaryTree()
     for i in range(1, 11):
-        test.insert(i)
+        test.insert(i, i ** 2)
 
     print_level_order(test.root)
     print("")
     print_post_order(test.root)
+    print("")
+    # print(test.get(11))
+    print(test.get(7).val)
+    print(test.contains(6))
